aac7c15de934e4fb07a1fd1e4a20c897ca750f54
Eduardo Born <nosachamos@gmail.com>
2022-09-18 12:25:30 -0500

    Small adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,6 @@ module.exports = {
         libraryTarget: 'umd'
     },
     externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react'
-        },
     },
     module: {
         rules: [
